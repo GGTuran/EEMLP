99689794a0c51b7a48bfc53eb829395278c8e5ca
GGTuran <tayyabturan67@gmail.com>
2025-09-05 19:29:47 +0330

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,11 +1,13 @@
 import os
 import sys
 sys.path.append(os.path.join(os.getcwd(), "src"))
-from src.exception import CustomException
-from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.logger import logging
+from src.exception import CustomException
+from src.components.data_transformation import DataTransformation_config
+from src.components.data_transformation import DataTransformation
 
 @dataclass
 
@@ -21,7 +23,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
          logging.info("Entered the data ingestion method or component")
          try:
-            df = pd.read_csv('notebook/data/stud.csv')
+            df = pd.read_csv('Notebook/data/stud.csv')
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok = True)
@@ -43,3 +45,13 @@ class DataIngestion:
 
          except Exception as e:
             raise CustomException(e,sys)
+         
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+    print(f"Train data saved to: {train_data}")
+    print(f"Test data saved to: {test_data}")
+
+    data_transformation = DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
