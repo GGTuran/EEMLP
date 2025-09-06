0ee652ae1dc7a1b98e66dfacaf325bd68f1971c4
GGTuran <tayyabturan67@gmail.com>
2025-09-06 13:17:10 +0330

    model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,8 @@ from src.logger import logging
 from src.exception import CustomException
 from src.components.data_transformation import DataTransformation_config
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 
@@ -55,3 +57,7 @@ if __name__ == "__main__":
 
     data_transformation = DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
+
